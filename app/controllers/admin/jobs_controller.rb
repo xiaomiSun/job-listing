418ae985281e13732ee8389799e26ee2827020a6
sunxiaomi <sunhemin@outlook.com>
2017-01-22 11:01:12 +0800

    set admin

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -1,14 +1,8 @@
 class Admin::JobsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
   before_action :require_is_admin
+  
   layout "admin"
-
-  def require_is_admin
-    if !current_user.admin?
-      flash[:alert] = 'You are not admin'
-      redirect_to root_path
-    end
-  end
 
   def show
     @job = Job.find(params[:id])
