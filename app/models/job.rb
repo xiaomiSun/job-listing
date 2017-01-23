8f4149c229bf8ea6f0347072b0b7305f0a0336ae
sunxiaomi <sunhemin@outlook.com>
2017-01-23 20:55:10 +0800

    ruby beautify job.rb&jobs_controller.rb

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,21 +1,25 @@
 class Job < ApplicationRecord
-  has_many :resumes
-  
-  validates :title, presence: true
-  validates :wage_upper_bound, presence: true
-  validates :wage_lower_bound, presence: true
-  validates :wage_lower_bound, numericality: { greater_than: 0}
+    has_many :resumes
 
-scope :published, -> { where(is_hidden: false) }
-scope :recent, -> { order('created_at DESC') }
+    validates :title, presence: true
+    validates :wage_upper_bound, presence: true
+    validates :wage_lower_bound, presence: true
+    validates :wage_lower_bound, numericality: { greater_than: 0 }
 
-  def publish!
-    self.is_hidden = false
-    self.save
-  end
+    scope :published, -> { where(is_hidden: false) }
+    scope :recent, -> { order('created_at DESC') }
 
-  def hide!
-    self.is_hidden = true
-    self.save
+    def publish!
+        self.is_hidden = false
+        save
+    end
+
+    def hide!
+        self.is_hidden = true
+        save
+    end
+
+    def self.search(search)
+        where('title LIKE ?', "%#{search}%").or(where('description LIKE ?', "%#{search}%"))
   end
 end
